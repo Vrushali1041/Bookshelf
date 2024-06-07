b857f284f8ae260791e0ac322fc789acc5752397
Vrushali Patil <vrushalimpatil2001@gmail.com>
2024-06-07 19:55:44 +0530

    code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import BookSearchPage from './pages/BookSearchPage';
+// import BookshelfPage from './pages/BookshelfPage';
+import BooksearchPage from './Pages/BooksearchPage'
+import BookshelfPage from './Pages/BookshelfPage'
+// App component with routing setup
+const App = () => (
+  <Router>
+    <Routes>
+      <Route exact path="/" element={<BooksearchPage />} />
+      <Route path="/bookshelf" element={<BookshelfPage />} />
+    </Routes>
+  </Router>
+);
 
 export default App;
